8e4b2b4bcbea8f4b2e6a24e1b45e891e72ce2997
Alice (VoevodovaNA) <voevodova02@gmail.com>
2021-03-22 16:51:16 +0300

    code: добвлена печать деления

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main()
     cout << a << " " << b;
     cout << "A + B = " << a + b << '\n';
     cout << "A - B = " << a - b << '\n';
-	cout << "A * B = " << a * b << '\n';
+    cout << "A * B = " << a * b << '\n';
+    cout << "A / B = " << a / b << '\n';
 
 return 0;
 }
