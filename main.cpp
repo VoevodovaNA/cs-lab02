b38ef74cb757cb5aec61d6ebf0d60c36a8654565
Alice (VoevodovaNA) <voevodova02@gmail.com>
2021-03-22 16:59:40 +0300

    code: добавлена печать максимума

--- main.cpp
+++ main.cpp
@@ -9,9 +9,14 @@ int main()
     cin >> a >> b;
     cout << a << " " << b;
     cout << "A + B = " << a + b << '\n';
+    max=1;
+    if (a+b>max)
+        max=a+b;
     cout << "A - B = " << a - b << '\n';
     cout << "A * B = " << a * b << '\n';
+    if (a*b>max)
+        max=a*b;
     cout << "A / B = " << a / b << '\n';
-
+    cout << max << '\n';
 return 0;
 }
