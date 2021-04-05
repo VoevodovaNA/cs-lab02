f0db98506f709834b9938fb079bdf06c8e3f1b4b
u211-12 <u211-12@public.mpei.local>
2021-04-05 14:02:55 +0300

    code: добавлена печать максимума

--- main.cpp
+++ main.cpp
@@ -5,18 +5,19 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b;
+    int a, b, max, min;
+    max=0;
+    min=0;
     cin >> a >> b;
     cout << a << " " << b;
     cout << "A + B = " << a + b << '\n';
-    max=1;
-    if (a+b>max)
-        max=a+b;
     cout << "A - B = " << a - b << '\n';
     cout << "A * B = " << a * b << '\n';
-    if (a*b>max)
-        max=a*b;
     cout << "A / B = " << a / b << '\n';
+    if (a+b>max)
+        max=a+b;
+    else if (a*b>max)
+        max=a*b;
     cout << max << '\n';
 return 0;
 }
